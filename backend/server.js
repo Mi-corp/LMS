8f275537ee102420852a78f54fa20d43b0af9efb
ChasteCR8 <chastecr8@gmail.com>
2024-12-02 16:37:44 +0200

    commiting

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
-const { domainToASCII, domainToUnicode } = require('url'); // Import domainToASCII and domainToUnicode from the url module
+const { domainToASCII, domainToUnicode } = require('url');
 const bookRoutes = require('./routes/bookRoutes');
 const borrowerRoutes = require('./routes/borrowerRoutes');
 const authorRoutes = require('./routes/authorRoutes');
@@ -12,13 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Serve the static HTML file
+app.use(express.static('public'));
+
+// Health check route
+app.get('/status', (req, res) => {
+  res.status(200).send({ status: 'Server is live!' });
+});
+
 // Connect to MongoDB
 mongoose.connect(`${process.env.MONGODB_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  dbName: 'Library_DB' //You may change the database name
+  dbName: 'Library_DB',
 });
 const db = mongoose.connection;
+
+db.on('error', (error) => console.error('MongoDB connection error:', error));
+db.once('open', () => console.log('Connected to MongoDB'));
 
 // Example Punycode operations using url module
 const unicodeDomain = domainToUnicode('xn--example-9h2c.com');
